aee07f6ab5b3124270190ddf89d891252996e043
nagisora <nagisora.icanspeng@gmail.com>
2024-07-18 21:04:41 +0900

    ファイル整理

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { useEffect, useState } from "react";
-
-export const App = () => {
-    return (
-        <div>
-            <h1 style={{ color: "red"}}>こんにちは</h1>          
-        </div>
-    )
-}
