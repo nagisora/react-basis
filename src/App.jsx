4bd91847cf07a32e0451a1e8c868da5e96bb6a12
nagisora <nagisora.icanspeng@gmail.com>
2024-07-20 08:02:42 +0900

    ファイルを最小限の記述に初期化した

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,7 @@
-import { useEffect, useState } from "react";
-import { ColorfulMessage } from "./components/ColorfulMessage";
-
 export const App = () => {
-    const [num, setNum] = useState(0);
-    const [isShowFace, setIsShowFace] = useState(true);
-    const onClickButton = () => {
-        setNum((prev) => prev + 1);
-    };
-    const contentStyle = {
-        color: "blue",
-    }
-    const onClickToggle = () => {
-        setIsShowFace(!isShowFace);
-    }
-
-    useEffect(() => {
-        if (num > 0) {
-            if (num % 3 === 0) {
-                isShowFace || setIsShowFace(true);
-            } else {
-                isShowFace && setIsShowFace(false);
-            }
-        }
-    }, [num]);
-
     return (
         <div>
-            <h1 style={{ color: "red"}}>こんにちは</h1>
-            <p style={contentStyle}>お元気aaaa？</p>
-            {console.log("ooo")}
-            <ColorfulMessage color="green" message="やあ">aaaa</ColorfulMessage>
-            <button onClick={onClickButton}>ボタン</button>
-            <p>{num}</p>
-            <button onClick={onClickToggle}>on/off</button>
-            {isShowFace && <p>＼(^o^)／</p>}
-            
+            <h1>helloworld</h1>
         </div>
     )
 }
