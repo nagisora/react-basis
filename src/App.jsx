36cbca31f5dfee94937275fd7712fd534fc81fd3
nagisora <nagisora.icanspeng@gmail.com>
2024-07-20 09:37:34 +0900

    再レンダリングの実験

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { useCallback, useState } from "react"
+import { ChildArea } from "./component/ChildArea";
+
 export const App = () => {
+    console.log("APP");
+
+    const [text, setText] = useState("");
+    const [open, setOpen] = useState(false);
+
+    const onChangeText = (e) => setText(e.target.value);
+
+    const onClickOpen = () => setOpen(!open);
+
+    const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+
     return (
         <div>
-            <h1>helloworld</h1>
+            <input value={text} onChange={onChangeText} />
+            <br />
+            <br />
+            <button onClick={onClickOpen}>表示</button>
+            <ChildArea open={open} onClickClose={onClickClose} />
         </div>
     )
 }
